85b5cbb648802dc55fd7a0efdd774cf5516b647c
shenhaohao-eng <shenhaohao@gmail.com>
2025-08-25 21:08:54 -0500

    Initial index.html upload; no media

---
+++ script.js
@@ -0,0 +1,7 @@
+//let numButtonClicks = 0;
+function buttonClicked() {
+    //numButtonClicks = numButtonClicks + 1;
+    document.getElementById("exbuttonDiv").textContent =
+    //    "And a " + numButtonClicks;
+        "Because they don't have the guts!";
+}
